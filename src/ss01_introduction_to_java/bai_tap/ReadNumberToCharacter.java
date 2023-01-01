deb9453b4fa5bd085d6de1d9fa002d3459599693
hoanghaibk93 <hoanghaibk93@gmail.com>
2023-01-01 10:42:51 +0700

    update ss1

--- src/ss01_introduction_to_java/bai_tap/ReadNumberToCharacter.java
+++ src/ss01_introduction_to_java/bai_tap/ReadNumberToCharacter.java
@@ -12,7 +12,9 @@ public class ReadNumberToCharacter {
 
         System.out.println("Enter a number");
         number = sc.nextInt();
-        if (number < 10 && number >= 0) {
+        if (number >= 1000 || number < 0) {
+            System.out.println("out of ability");
+        } else if (number < 10 && number >= 0) {
             switch (number) {
                 case 0:
                     wordOne = "Zero";
@@ -49,7 +51,6 @@ public class ReadNumberToCharacter {
                     break;
                 default:
                     wordOne = "";
-                    System.out.println("out of ability");
                     break;
             }
             System.out.println(wordOne);
@@ -160,42 +161,76 @@ public class ReadNumberToCharacter {
             int tens = (number % 100) / 10;
             int twos = number / 100;
 
-            switch (ones) {
-                case 1:
-                    wordOne = "one";
-                    break;
-                case 2:
-                    wordOne = "two";
-                    break;
-                case 3:
-                    wordOne = "three";
-                    break;
-                case 4:
-                    wordOne = "four";
-                    break;
-                case 5:
-                    wordOne = "five";
-                    break;
-                case 6:
-                    wordOne = "six";
-                    break;
-                case 7:
-                    wordOne = "seven";
-                    break;
-                case 8:
-                    wordOne = "eight";
-                    break;
-                case 9:
-                    wordOne = "nine";
-                    break;
-                default:
-                    wordOne = "";
+            if (tens == 1) {
+                switch (ones) {
+                    case 0:
+                        wordOne = "Ten";
+                        break;
+                    case 1:
+                        wordOne = "Eleven";
+                        break;
+                    case 2:
+                        wordOne = "Twelve";
+                        break;
+                    case 3:
+                        wordOne = "Thirteen";
+                        break;
+                    case 4:
+                        wordOne = "Fourteen";
+                        break;
+                    case 5:
+                        wordOne = "Fifteen";
+                        break;
+                    case 6:
+                        wordOne = "Sixteen";
+                        break;
+                    case 7:
+                        wordOne = "Seventeen";
+                        break;
+                    case 8:
+                        wordOne = "Eighteen";
+                        break;
+                    case 9:
+                        wordOne = "Nineteen";
+                        break;
+                    default:
+                        wordOne = "";
+                        break;
+                }
+            } else {
+                switch (ones) {
+                    case 1:
+                        wordOne = "one";
+                        break;
+                    case 2:
+                        wordOne = "two";
+                        break;
+                    case 3:
+                        wordOne = "three";
+                        break;
+                    case 4:
+                        wordOne = "four";
+                        break;
+                    case 5:
+                        wordOne = "five";
+                        break;
+                    case 6:
+                        wordOne = "six";
+                        break;
+                    case 7:
+                        wordOne = "seven";
+                        break;
+                    case 8:
+                        wordOne = "eight";
+                        break;
+                    case 9:
+                        wordOne = "nine";
+                        break;
+                    default:
+                        wordOne = "";
+                }
             }
-
             switch (tens) {
-                case 1:
-                    wordTwo = "Ten";
-                    break;
                 case 2:
                     wordTwo = "Twenty";
                     break;
@@ -223,6 +258,8 @@ public class ReadNumberToCharacter {
                 default:
                     wordTwo = "";
             }
+
+
             switch (twos) {
                 case 1:
                     wordThree = "One hundred";
@@ -254,9 +291,16 @@ public class ReadNumberToCharacter {
                 default:
                     wordThree = "";
             }
-            System.out.printf(wordThree + " and " + wordTwo + " " + wordOne);
+            if (tens == 1) {
+                System.out.println(wordThree + " and " + " " + wordOne);
+            } else if (ones == 0 && tens == 0) {
+                System.out.println(wordThree);
+            } else {
+                System.out.printf(wordThree + " and " + wordTwo + " " + wordOne);
+            }
         }
     }
 }
+
 
 
