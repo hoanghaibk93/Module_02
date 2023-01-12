ec59a12368e1a88054a1a6a6cb3f370369814640
hoanghaibk93 <hoanghaibk93@gmail.com>
2023-01-12 21:09:33 +0700

    update ss9

--- src/ss09_dsa_list/bai_tap/linkedlist/MyLinkedListTest.java
+++ src/ss09_dsa_list/bai_tap/linkedlist/MyLinkedListTest.java
@@ -2,16 +2,15 @@ package ss09_dsa_list.bai_tap.linkedlist;
 
 public class MyLinkedListTest {
     public static void main(String[] args) {
-        MyLinkedList myLinkedList = new MyLinkedList(25);
-        myLinkedList.addFirst(myLinkedList);
-        myLinkedList.addFirst(12);
-        myLinkedList.addFirst(13);
-        myLinkedList.add(4, 9);
-        myLinkedList.add(4, 9);
-        myLinkedList.remote(3);
-        myLinkedList.remoteFirst();
+        MyLinkedList myLinkedList = new MyLinkedList("Hai");
+        myLinkedList.addFirst("Hoang");
+        myLinkedList.addFirst("Hanh");
+        myLinkedList.add(4, "Huy");
+        myLinkedList.add(0, "Tuan");
+        myLinkedList.addFirst("Tung");
+        myLinkedList.addFirst("Cong");
+        myLinkedList.remote(2);
         myLinkedList.remoteLast();
-        System.out.println(myLinkedList);
-        //myLinkedList.printList();
+        myLinkedList.printList();
     }
 }
