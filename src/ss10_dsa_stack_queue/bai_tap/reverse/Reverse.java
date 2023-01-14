186c98eb3d739c91e97e70357c7e776373edb2d0
hoanghaibk93 <hoanghaibk93@gmail.com>
2023-01-14 13:29:46 +0700

    update ss10

--- src/ss10_dsa_stack_queue/bai_tap/reverse/Reverse.java
+++ src/ss10_dsa_stack_queue/bai_tap/reverse/Reverse.java
@@ -1,17 +1,25 @@
 package ss10_dsa_stack_queue.bai_tap.reverse;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class Reverse {
     public static void main(String[] args) {
-        System.out.println("Array number");
-        Stack<Integer> stackNumber = new Stack<>();
-        int[] arrayNumber = {1, 2, 3, 4, 5, 6};
-        System.out.println("Before reverse:");
-        for (int i = 0; i < arrayNumber.length; i++) {
-            System.out.print(arrayNumber[i] + "\t");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a length of number Array:");
+        int lengthArray = Integer.parseInt(scanner.nextLine());
+        int[] arrayNumber = new int[lengthArray];
+        for (int i = 0; i < lengthArray; i++) {
+            System.out.println("Enter elements " + (i + 1) + " of Array");
+            arrayNumber[i] = Integer.parseInt((scanner.nextLine()));
         }
+        System.out.println("Enter a String: ");
+        String word = scanner.nextLine();
+
+        Stack<Integer> stackNumber = new Stack<>();
+        System.out.println("Before reverse:");
+        System.out.println(Arrays.toString(arrayNumber));
         System.out.println("\nAfter reverse");
         for (int i = 0; i < arrayNumber.length; i++) {
             stackNumber.push(arrayNumber[i]);
@@ -19,13 +27,10 @@ public class Reverse {
         for (int i = 0; i < arrayNumber.length; i++) {
             arrayNumber[i] = stackNumber.pop();
         }
-        for (int i = 0; i < arrayNumber.length; i++) {
-            System.out.print(arrayNumber[i] + "\t");
-        }
+        System.out.println(Arrays.toString(arrayNumber));
 
         Stack<String> stackString = new Stack<>();
         System.out.println("\nBefore reverse:");
-        String word = "hoanghai";
         System.out.println(word);
         String[] arrayChar = word.split("");
         for (int i = 0; i < arrayChar.length; i++) {
@@ -34,9 +39,7 @@ public class Reverse {
         for (int i = 0; i < arrayChar.length; i++) {
             arrayChar[i] = stackString.pop();
         }
-        System.out.println("\nAfter reverse");
-        for (int i = 0; i < arrayChar.length; i++) {
-            System.out.print(arrayChar[i]);
-        }
+        System.out.println("\nAfter reverse:");
+        System.out.println(String.join("", arrayChar));
     }
 }
--- src/ss10_dsa_stack_queue/thuc_hanh/bai1/Palindrome.java
+++ src/ss10_dsa_stack_queue/thuc_hanh/bai1/Palindrome.java
@@ -1,41 +1,29 @@
 package ss10_dsa_stack_queue.thuc_hanh.bai1;
 
-import ss04_oop_class_object.bai_tap.quadraticequation.QuadraticEquation;
 
-import javax.imageio.plugins.tiff.TIFFImageReadParam;
 import java.util.*;
 
 public class Palindrome {
     public static void main(String[] args) {
-
-        String stringWord = "Able was I ere I saw Elba";
-        String word = stringWord.toLowerCase();
-        //System.out.println(word);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a String that you want to check: ");
+        String stringWord = scanner.nextLine();
+        String stringWordNew = stringWord.toLowerCase();
         Queue<String> queue = new LinkedList<>();
-        String[] arrayChar = word.split("");
-        String[] arrayChar2 = word.split("");
-        for (int i = 0; i < arrayChar.length; i++) {
-            queue.add(arrayChar[i]);
-        }
-        for (int i = 0; i < arrayChar.length; i++) {
-            arrayChar[i] = queue.poll();
-        }
         Stack<String> stack = new Stack<>();
-
-        for (int i = 0; i < arrayChar2.length; i++) {
-            stack.push(arrayChar2[i]);
+        String[] arrayChar = stringWordNew.split("");
+        for (int i = 0; i < arrayChar.length; i++) {
+            queue.offer(arrayChar[i]);
+            stack.push(arrayChar[i]);
         }
-        for (int i = 0; i < arrayChar2.length; i++) {
-            arrayChar2[i] = stack.pop();
-        }
-        for (int i = 0; i < arrayChar2.length; i++) {
-            if (arrayChar[i] !== arrayChar2[i]){
-                return "false";
+        while (!queue.isEmpty()) {
+            if (!queue.poll().equals(stack.pop())) {
+                System.out.println("'" + stringWord + "'" + " is not a Palindrome String");
+                break;
+            } else if (queue.isEmpty()) {
+                System.out.println("'" + stringWord + "'" + " is a Palindrome String");
             }
-            return "true";
-
         }
-
-
     }
 }
+
